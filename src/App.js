8303b806d7830da62bf17c1cc5f9a84c0b1effa7
Sai Charan <charankuppa@gmail.com>
2017-05-13 14:56:42 +0530

    Get initial info

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerCount: null,
+      baseAmt: null,
+      bankBalance: 0
+    }
+    this.onPlayerCountChange = this.onPlayerCountChange.bind(this);
+    this.onBaseAmtChange = this.onBaseAmtChange.bind(this);
+  }
+
+  updateBankBalance() {
+    let bankBalance = this.state.playerCount * this.state.baseAmt;
+    this.setState({ bankBalance });
+  }
+
+  onPlayerCountChange(e) {
+    this.setState({ playerCount: e.target.value }, this.updateBankBalance);
+  }
+
+  onBaseAmtChange(e) {
+    this.setState({ baseAmt: e.target.value}, this.updateBankBalance);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +34,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Two Cards</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        <label htmlFor="player-count"> Enter the number of players</label>
+        <input type="number" id="player-count" onChange={this.onPlayerCountChange} />
+        </div>
+        <div>
+        <label htmlFor="base-amt"> Enter the base amount</label>
+        <input type="number" id="base-amt" onChange={this.onBaseAmtChange} />
+        </div>
+        <p>Current Bank balance: {this.state.bankBalance}</p>
       </div>
     );
   }
